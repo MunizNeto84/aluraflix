cfc4b677618e559da183eec1f4f8da6e48471d06
Muniz Neto <muniznetto@outlook.com>
2025-02-23 13:47:32 -0300

    ajustado  video main para sessao de busca.

--- src/components/pages/Home/Sections/Search.jsx
+++ src/components/pages/Home/Sections/Search.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Title, Subtitle } from "../../../../styles/Tittle/Tittle";
 import {
   SearchContainer,
@@ -6,6 +7,7 @@ import {
   VideoGrid,
 } from "../../../../styles/Div/Div";
 import { SearchThumbnail } from "../../../../styles/Img/Img";
+import VideoMain from "./VideoMain";
 
 const extractVideoId = (url) => {
   const regex =
@@ -15,26 +17,47 @@ const extractVideoId = (url) => {
 };
 
 const Search = ({ videosBuscados }) => {
+  const [showVideo, setShowVideo] = useState(false);
+  const [videoSelecionado, setVideoSelecionado] = useState(null);
+
   if (videosBuscados.length === 0) return null;
 
   return (
     <SearchContainer>
       <Title>Resultados da Busca</Title>
       <VideoGrid>
-        {videosBuscados.map((video) => (
-          <VideoCard key={video.id}>
-            <Subtitle>{video.titulo}</Subtitle>
-            <CategoriaVideoThumbnail>
-              <SearchThumbnail
-                src={`https://img.youtube.com/vi/${extractVideoId(
-                  video.url
-                )}/mqdefault.jpg`}
-                alt={video.titulo}
-              />
-            </CategoriaVideoThumbnail>
-          </VideoCard>
-        ))}
+        {videosBuscados.map((video) => {
+          const videoId = extractVideoId(video.url);
+
+          return (
+            <VideoCard key={video.id}>
+              <Subtitle>{video.titulo}</Subtitle>
+              <CategoriaVideoThumbnail>
+                <SearchThumbnail
+                  src={`https://img.youtube.com/vi/${videoId}/mqdefault.jpg`}
+                  alt={video.titulo}
+                  onClick={() => {
+                    setVideoSelecionado(video);
+                    setShowVideo(true);
+                  }}
+                />
+              </CategoriaVideoThumbnail>
+            </VideoCard>
+          );
+        })}
       </VideoGrid>
+
+      {videoSelecionado && (
+        <VideoMain
+          videoUrl={extractVideoId(videoSelecionado.url)}
+          showVideo={showVideo}
+          setShowVideo={setShowVideo}
+          idCategoria={videoSelecionado.categoriaId}
+          idCanal={videoSelecionado.canalId}
+          titulo={videoSelecionado.titulo}
+          descricao={videoSelecionado.descricao}
+        />
+      )}
     </SearchContainer>
   );
 };
